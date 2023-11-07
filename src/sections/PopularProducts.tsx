9fa6c7b6616d2054f8669655e63304b4661143a9
hariyebk <harunbekri6@gmail.com>
2023-11-07 18:43:35 +0300

    File and Folder structure, project setup

---
+++ src/sections/PopularProducts.tsx
@@ -0,0 +1,6 @@
+
+export default function PopularProducts() {
+    return (
+        <div>PopularProducts</div>
+    )
+}
---
+++ src/sections/SuperQuality.tsx
@@ -0,0 +1,6 @@
+
+export default function SuperQuality() {
+    return (
+        <div>SuperQuality</div>
+    )
+}
