678b3ca46049f7adc35c855c93cd33c6cc7d303d
hariyebk <harunbekri6@gmail.com>
2023-11-08 18:34:35 +0300

    popular products section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -21,8 +21,8 @@ export default function Hero() {
             {/* New Arrival text */}
             <div className="relative xl:w-2/5 flex flex-col gap-5 justify-center items-start w-full max-xl:padding-x pt-28">
                 <p className="text-xl font-montserrat text-coral-red"> Our popular collection</p>
-                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-                <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10"> The New Ariival </span> <br/>
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+                <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10"> The New Arival </span> <br/>
                 <span className="text-coral-red inline-block mt-3"> Nike </span> Shoes
                 </h1>
                 <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm"> Discover stylish Nike arrivals, quality comfort, and innovation for your active life</p>
--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -1,6 +1,32 @@
-
+import PopularProductCard from "../components/PopularProductCard "
+import {products} from "../constants/"
+export interface PopularProductsProps {
+    imgURL: string,
+    name: string,
+    price: string,
+    rating: string
+}
 export default function PopularProducts() {
     return (
-        <div>PopularProducts</div>
+        <section id="products" className="max-container max-sm:mt-12">
+            <div className="flex flex-col justify-start gap-5">
+                <h2 className="text-4xl font-palanquin font-bold"> Our <span className="text-coral-red"> Popular </span> products </h2>
+                <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray"> Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design and value.</p>
+            </div>
+            {/* products */}
+            <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+                {
+                    products.map((product: PopularProductsProps):React.ReactNode => {
+                        return (
+                            <div key={product.name}>
+                                <PopularProductCard imgURL={product.imgURL} name={product.name} price={product.price} rating = {product.rating}/>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+
+        </section>
     )
 }
