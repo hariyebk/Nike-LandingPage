d98e9285f549403f2247db4c9be23628e204983a
hariyebk <harunbekri6@gmail.com>
2023-11-09 11:22:48 +0300

    contact-us section

--- src/sections/Subscribe.tsx
+++ src/sections/Subscribe.tsx
@@ -1,6 +1,15 @@
+import Button from "../components/Button";
 
 export default function Subscribe() {
     return (
-        <div>Subscribe</div>
+        <section id="contact-us" className=" max-container flex flex-1 max-lg:flex-col gap-10 justify-between items-center">
+            <h3 className="text-4xl leading-[68px] font-palanquin lg:max-w-md font-bold "> Sign up for <span className="text-coral-red"> Updates </span> & NewsLetter</h3>
+            <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+                <input type="text" placeholder="subscribe@nike.com" className="input" />
+                <div className="flex max-sm:justify-end items-center max-sm:w-full">
+                    <Button label="Sign Up" />
+                </div>
+            </div>
+        </section>
     )
 }
