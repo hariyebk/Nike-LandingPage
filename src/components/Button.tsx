c2413bca063bf314b9e9d12f6f891f4d5a97116c
hariyebk <harunbekri6@gmail.com>
2023-11-09 08:17:31 +0300

    special offer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
 
 interface ButtonProps {
     label: string,
-    iconUrl?: string
+    iconUrl?: string,
+    bgColor?: string,
+    borderColor?: string,
+    textColor?: string
 }
-export default function Button({label, iconUrl}: ButtonProps) {
+export default function Button({label, iconUrl, bgColor, borderColor, textColor}: ButtonProps) {
     return (
-        <button className=" flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border-2 ${borderColor && borderColor}font-montserrat text-lg leading-none ${bgColor ? bgColor : "bg-coral-red"} rounded-full ${textColor ? textColor : "text-white"}`}>
             {label}
             { iconUrl && <img src= {iconUrl}  alt = "cta-button" className="ml-2 rounded-full w-5 h-5" />}
         </button>
--- src/sections/SpecialOffers.tsx
+++ src/sections/SpecialOffers.tsx
@@ -1,7 +1,23 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
 
 
 export default function SpecialOffers() {
     return (
-        <div>SpecialOffers</div>
+        <section className="flex items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div className="flex-1">
+                <img src={offer} width={773} height={687} className="object-contain w-full" />
+            </div>
+            <div className="flex flex-1 flex-col">
+                    <h2 className="text-4xl font-palanquin font-bold capitalize lg:max-w-lg"> <span  className="text-coral-red"> Special </span> Offer </h2>
+                    <p className="lg:max-w-lg mt-6 font-montserrat text-justify text-slate-gray leading-7 "> Embark on  a shopping journey that redefines your experience with unbeatable deals. From premier selections to increadible savings, we offer unparalleled value that sets us apart. </p>
+                    <p className="lg:max-w-lg mt-4 font-montserrat text-slate-gray leading-7"> Navigate a realm of posibilities designed to fullfill your unique desires, surpasssing the loftiest expectations. your journey with us is nothing short of exceptional.</p>
+                    <div className=" flex flex-wrap gap-4 mt-11">
+                        <Button label="Shop now" iconUrl={arrowRight} />
+                        <Button label="Leran more" bgColor = "bg-white" borderColor = "border-slate-gray" textColor = "tet-slate-gray"/> 
+                    </div>
+            </div>
+        </section>
     )
 }
